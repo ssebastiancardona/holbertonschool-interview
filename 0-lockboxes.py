4eb5da6a2f41d6db9a5b417884e146ab3e9effc8
ssebastiancardona <ssebastiancardona@gmail.com>
2021-12-09 17:47:22 -0500

    create def

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 """This is a test to get the jop. got luck madrefoca"""
+def canUnlockAll(boxes):
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
