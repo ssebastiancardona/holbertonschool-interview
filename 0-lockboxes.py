0385e5b0c866fac5ef05ca15004b306b60f9eeec
ssebastiancardona <ssebastiancardona@gmail.com>
2021-12-09 17:52:11 -0500

    Comment lines

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This is a test to get the jop. got luck madrefoca"""
+
+
 def canUnlockAll(boxes):
     if type(boxes) is not list:
         return False
--- main_0.py
+++ main_0.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""this is a coment"""
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
