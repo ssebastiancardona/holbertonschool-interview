ea5d1858e18eacfc6993496873f7a5d93709afbd
ssebastiancardona <ssebastiancardona@gmail.com>
2022-01-06 20:43:32 -0500

    0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,3 +1,51 @@
 #!/usr/bin/python3
 """ Min Op"""
 def minOperations(n):
+    if not isinstance(n, int) or n <= 1:
+        return 0
+    inicio = 1  # characters in the text file start always in 1
+    contador = 0
+    while (inicio < n):
+        rest = n - inicio
+
+        if (rest % inicio == 0):
+            clipBoard = inicio
+            inicio += clipBoard
+            contador += 2
+        else:
+            inicio += clipBoard
+            contador = contador + 1
+
+    return (contador)
+
+
+def minimumSpanning(n, inicio, clipBoard, action, opt):
+    """
+    will go through the options to find the shortest
+    The proble is that consume to much space, seem to be an infinite recursion
+    """
+
+    if inicio == n:
+        return opt
+    elif inicio > n:
+        return 0
+
+    if action == "CopyPaste":
+        clipBoard = inicio
+        inicio += clipBoard
+        opt += 2
+    elif action == "Paste":
+        inicio += clipBoard
+        opt += 1 #the incremental variable
+
+    copiar = minimumSpanning(n, inicio, clipBoard, "CopyPaste", opt)
+    pegar = minimumSpanning(n, inicio, clipBoard, "Paste", opt)
+
+    if (copiar != 0 and pegar != 0 and copiar <= pegar) or (
+         copiar != 0 and pegar == 0):
+        return copiar
+    elif (copiar != 0 and pegar != 0 and pegar <= copiar) or (
+         copiar == 0 and pegar != 0):
+        return pegar
+    else:
+        return 0
