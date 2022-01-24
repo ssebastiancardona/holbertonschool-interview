8036c204d048c563f64a28e9b128c2ef527d8ede
ssebastiancardona <ssebastiancardona@gmail.com>
2022-01-23 23:09:11 -0500

    fixed

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+    listint_t *low = *head;
+    listint_t *fast = *head;
+    int i = 0;
+
+    if (!*head || !(*head))
+    {
+        return (1);
+    }
+    if (*head != NULL && (*head)->next != NULL)
+    {
+        while (fast != NULL && fast->next != NULL)
+        {
+                if (low->n != fast->next->n)
+                {
+                    return (1);
+                }
+                i++;//++
+        }
+    }
+    return (0);
+}
