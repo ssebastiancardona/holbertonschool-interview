482443fd0d1a60f7f75e151cf318d744e5b8c7ca
ssebastiancardona <ssebastiancardona@gmail.com>
2022-01-27 22:53:10 -0500

    0-insert_number.c

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,45 +1,38 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
-
 /**
- * insert_node - insert elements on a listint_t list
- * @head: pointer to head of list
- * @number: value
- * Return: the address of the new node, or NULL if it failed
+ * insert_node - inserta un nuevo nodo
+ * @head: puntero
+ * @number: numero
+ * Return: NULL
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *newN;
-	listint_t *Cn;
+	listint_t *actual;
+	listint_t *nuevo = malloc(sizeof(listint_t));
 
-	/* defining the start of the list */
-	Cn = *head;
-	/* Allocate Memory to get a new Node*/
-	newN = malloc(sizeof(listint_t));
+	if (!nuevo)
+		return (NULL);
+	nuevo->n = number;
 
-	/** Validate if newNode isn't Null **/
-    
-	if (newN)
+	if (*head == NULL || (*head)->n >= nuevo->n)
 	{
-		newN->n = number;
-		newN->next = NULL;
-		if (*head == NULL || (*head)->n >= newN->n)
-		{
-			newN->next = *head;
-			*head = newNode;
-		}
-		else
-		{
-			while (Cn->next && Cn->next->n < newNode->n)
-			{
-				Cn = Cn->next;
-			}
-			newNode->next = Cn->next;
-			Cn->next = newN;
-		}
-		return (newN);
+		nuevo->next = *head;
+		*head = nuevo;
 	}
-	return (NULL);
+	else
+	{
+		actual = *head;
+		while (actual->next != NULL && actual->next->n < nuevo->n)
+		{
+			actual = actual->next;
+		}
+		nuevo->next = actual->next;
+		actual->next = nuevo;
+	}
+	return (nuevo);
 }
