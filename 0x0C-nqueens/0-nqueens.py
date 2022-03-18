0cb8c39846ac0883ddb8aac14c362adc6ea6b7a6
ssebastiancardona <ssebastiancardona@gmail.com>
2022-03-17 22:58:59 -0500

    0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,46 +1,24 @@
 #!/usr/bin/python3
 """ commet to """
-from sys import argv, exit
+import sys
+
+""" commet to """
+def nqueens(n: int):
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    print(str(matrix))
+
 
 if __name__ == "__main__":
-
-    if len(argv) != 2:
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
         print("Usage: nqueens N")
         exit(1)
 
-    queens = argv[1]
-
-    try:
-        queens = int(queens)
-    except ValueError:
+    if not sys.argv[1].isdigit():
         print("N must be a number")
         exit(1)
 
-    if queens < 4:
+    if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         exit(1)
 
-    sol = []
-
-    def nqueens(row, queens, sol):
-        """ commet to """
-        if (row == queens):
-            print(sol)
-        else:
-            for col in range(queens):
-                pos = [row, col]
-                if validposition(sol, pos):
-                    sol.append(pos)
-                    nqueens(row + 1, queens, sol)
-                    sol.remove(pos)
-
-    def validposition(solution, position):
-        """ commet to """
-        for n in solution:
-            if n[1] == position[1] or \
-                (n[0] - n[1]) == (position[0] - position[1]) or \
-                    (n[0] + n[1]) == (position[0] + position[1]):
-                return False
-        return True
-
-    nqueens(0, queens, sol)
+    nqueens(int(sys.argv[1]))
