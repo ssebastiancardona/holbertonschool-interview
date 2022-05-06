28fa72d0eca8997c2bcb12fc25689aa85d23f112
ssebastiancardona <ssebastiancardona@gmail.com>
2022-05-05 23:51:27 -0500

    0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -6,40 +6,40 @@ from requests import request
 def generate_dicts(word_list):
     """ Lines of code """
     contador = {k: 0 for k in word_list}
-    dup = {}
+    dupo = {}
     for k in word_list:
-        if k not in dup:
-            dup[k] = 0
-        dup[k] += 1
-    return (contador, dup)
+        if k not in dupo:
+            dupo[k] = 0
+        dupo[k] += 1
+    return (contador, dupo)
 
 
-def count_words(subreddit, word_list, after="", contador={}, dup={}, init=0):
+def count_words(subreddit, word_list, after="", count={}, dup={}, init=0):
     """ Lines of code """
     if not init:
-        contador, dup = generate_dicts(word_list)
+        count, dup = generate_dicts(word_list)
 
     url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
-    head = {"User-Agent": "Python3"}
-    response = request("GET", url, head=head).json()
+    headers = {"User-Agent": "Python3"}
+    response = request("GET", url, headers=headers).json()
     try:
         dat = response.get('data')
         top = dat.get('children')
         _after = dat.get('after')
 
         for item in top:
-            data = item.get('data')['title']
-            for word in contador:
-                amount = data.lower().split(' ').contador(word.lower())
-                contador[word] += amount
+            dat = item.get('data')['title']
+            for word in count:
+                amount = dat.lower().split(' ').count(word.lower())
+                count[word] += amount
 
         if _after:
-            count_words(subreddit, word_list, _after, contador, dup, 1)
+            count_words(subreddit, word_list, _after, count, dup, 1)
         else:
-            sort_abc = sorted(contador.items(), key=lambda tup: tup[::-1])
-            desc = sorted(sort_abc, key=lambda tup: tup[1], reverse=True)
+            sort_abc = sorted(count.items(), key=lambda tup: tup[::-1])
+            desco = sorted(sort_abc, key=lambda tup: tup[1], reverse=True)
 
-            for name, cnt in desc:
+            for name, cnt in desco:
                 cnt *= dup[name]
                 if cnt:
                     print('{}: {}'.format(name.lower(), cnt))
