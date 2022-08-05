4c049d23ed138de713a483e6a072a4ef40bb8733
ssebastiancardona <ss3bastiancardona@gmail.com>
2022-08-04 23:02:05 -0500

    0-pascal_triangle.py

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+una forma de resolver el problema de pascual
+"""
+
+
+def pascal_triangle(n):
+    """retorna una lista de listas pascuales con el trioangulo de pascual"""
+    if n <= 0:
+        return []
+
+    list_pascuales = []
+
+    for i in range(n):
+        list_pascuales.append([])
+        list_pascuales[i].append(1)
+        for j in range(1, i):
+            list_pascuales[i].append(
+                list_pascuales[i - 1][j - 1] + list_pascuales[i - 1][j])
+        if i != 0:
+            list_pascuales[i].append(1)
+
+    return list_pascuales
